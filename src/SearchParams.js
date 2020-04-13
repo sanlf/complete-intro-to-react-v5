2f1e1a2502e5044d6e23dc9db667bf69087e346f
sanlf <sanlf@protonmail.com>
2020-04-13 13:38:33 -0700

    Hooks
    Code changes in the Hook lesson

---
+++ src/SearchParams.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+    const [location, updateLocation] = useState("Seattle, WA");
+    const [breeds, updateBreeds] = useState([]);
+    console.log(ANIMALS);
+
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+    return (
+        <div className="search-params">
+            <form action="">
+                <label htmlFor="location">
+                    Location
+                    <input
+                        id="location"
+                        value={location}
+                        placeholder="Location"
+                        onChange={e => updateLocation(e.target.value)}
+                    />
+                </label>
+
+                <AnimalDropdown />
+                <BreedDropdown />
+
+
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
+
